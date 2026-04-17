a7597099890d1053d5d7268fda816c481b43d6ab
hieucyrus0910-glitch <hieucyrus0910@gmail.com>
2026-04-18 02:10:34 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-$(document).ready(function() {
-    $(".nav button").click(function() {
-        $(".content").removeClass("active");
-        $("#" + $(this).attr('id')).addClass("active");
-    });
-});
